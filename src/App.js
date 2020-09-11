34b20be3d4731997b9144fa75f48097652f14e11
Eman <eman.cse2008@gmail.com>
2020-09-11 11:22:53 +0200

    props and state

--- src/App.js
+++ src/App.js
@@ -9,18 +9,65 @@ class App extends Component {
       { name: 'Mustafa', age: '30' },
       { name: 'Yahya', age: '2' },
       { name: 'Younes', age: '1' },
-    ]
+    ],
+    otherState: 'toggle ok'
   };
 
+  switchNameHandler =(newName) => {
+    console.log('Was clicked');
+    //DONT DO THIS : this.state.persons[0].name = 'Emyu';
+    this.setState({
+      persons: [
+        { name: newName, age: '33' },
+        { name: 'Mustafa', age: '30' },
+        { name: 'Yahya', age: '2' },
+        { name: 'Younes', age: '1' },
+      ]
+    });
+    this.setState({
+      otherState: 'Not ok'
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Eman changed', age: '33' },
+        { name: 'Mustafa', age: '30' },
+        { name: event.target.value, age: '2' },
+        { name: 'Younes changed', age: '1' },
+      ]
+    });
+  }
+
   render() {
+    const style= {
+      backgroundColor: 'white',
+      border: '1px solid red',
+      padding: '8px'
+    };
     return (
       <div className="App">
         <h1>Hi, I am react app</h1>
         <p>this is working</p>
-        <button>Switch name</button>
-        <Person name='Eman' age='34' />
-        <Person name='Mustafa' age='30' >My Hoppies Person</Person>
-        <Person name='Yahya' age='2' />
+        <p>{this.state.otherState}</p>
+        {/* if there is no parameters */}
+        {/* <button onClick={this.switchNameHandler}>Switch name</button> */}
+        <button style={style} onClick={() => this.switchNameHandler('Ahmed')}>Switch name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this,'Asmaa')} />
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
+        changed={this.nameChangedHandler} >My Hoppies Person</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} />
+        <Person 
+        name={this.state.persons[3].name} 
+        age={this.state.persons[3].age} />
       </div>
     );
   }
