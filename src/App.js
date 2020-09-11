590f5b040a8f753752acad9c35ad95fc1a1c72e0
Eman <eman.cse2008@gmail.com>
2020-09-11 14:23:50 +0200

    use radium for inline style

--- src/App.js
+++ src/App.js
@@ -1,87 +1,100 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Eman', age: '34' },
-      { name: 'Mustafa', age: '30' },
-      { name: 'Yahya', age: '2' },
-      { name: 'Younes', age: '1' },
+      { id: '1', name: 'Mustafa', age: '30' },
+      { id: '2', name: 'Yahya', age: '2' },
+      { id: '3', name: 'Younes', age: '1' },
     ],
     otherState: 'toggle ok'
   };
 
-  switchNameHandler = (newName) => {
-    console.log('Was clicked');
-    //DONT DO THIS : this.state.persons[0].name = 'Emyu';
-    this.setState({
-      persons: [
-        { name: newName, age: '33' },
-        { name: 'Mustafa', age: '30' },
-        { name: 'Yahya', age: '2' },
-        { name: 'Younes', age: '1' },
-      ],
-      showPersons: false
+  nameChangedHandler = (event, id) => {
+    // const personIndex = this.state.persons.find(p => p.id === id);
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
-    this.setState({
-      otherState: 'Not ok'
-    });
+    const person = { ...this.state.persons[personIndex] }; //this or next line to copy object
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons });
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Eman changed', age: '33' },
-        { name: 'Mustafa', age: '30' },
-        { name: event.target.value, age: '2' },
-        { name: 'Younes changed', age: '1' },
-      ]
-    });
-  }
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons;  this create pointer to original array not copy
+    // const persons = this.state.persons.slice(); use this Or 
+    const persons = [...this.state.persons]; //spread operator .. craete copy of persons
+
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  };
 
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
     const style = {
-      backgroundColor: 'white',
-      border: '1px solid red',
-      padding: '8px'
+      backgroundColor: 'green',
+      color: 'white',
+      border: '1px solid #eee',
+      padding: '8px',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
-    return (
-      <div className="App">
-        <h1>Hi, I am react app</h1>
-        <p>this is working</p>
-        <p>{this.state.otherState}</p>
-        {/* if there is no parameters */}
-        {/* <button onClick={this.switchNameHandler}>Switch name</button> */}
-        <button style={style} onClick={this.togglePersonHandler}>Switch name</button>
 
-        { this.state.showPersons ? 
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-              click={this.switchNameHandler.bind(this, 'Asmaa')} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              changed={this.nameChangedHandler} >My Hoppies Person</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-            <Person
-              name={this.state.persons[3].name}
-              age={this.state.persons[3].age} />
-          </div> : null
-        }
-      </div>
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              clicked={() => this.deletePersonHandler(index)}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+              name={person.name}
+              age={person.age}
+              key={person.id} />
+          })}
+        </div>
+      );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
+    }
+    // let classes = ['red', 'blue'].join(' ');
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+
+    }
+    return (
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I am react app</h1>
+          <p className={classes.join(' ')}>this is working</p>
+          <p>{this.state.otherState}</p>
+          <button style={style} onClick={this.togglePersonHandler}>Switch name</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
